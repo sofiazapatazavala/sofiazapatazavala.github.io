f4b1a3a4a6f65e7a3ba867e6412260fce0ab2271
Sofía Zapata Zavala <sofiazapatazavala@gmail.com>
2017-09-30 22:32:40 -0300

    bs4-b1, más manuscrita, polaroids

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,5 +1,7 @@
 // JS required by the theme to run smoothly. :)
 $(function() {
   $("img").addClass("img-fluid");
-  $("body.post article.post img").addClass("rounded img-thumbnail d-block mx-auto")
+  $("body.post article.post img").addClass("rounded img-thumbnail d-block mx-auto");
+  $("body.home div.card p:first-of-type").addClass("m-0");
+  $("body.home div.card img").addClass("card-img-top");
 });
