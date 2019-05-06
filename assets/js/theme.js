8fbcc215156aa22e81fc4bf85e05750b6f0baf2a
Sofía Zapata Zavala <sofiazapatazavala@gmail.com>
2019-05-06 15:20:11 -0400

    2019v1 shipped

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,5 +1,15 @@
 // JS required by the theme to run smoothly. :)
 $(function() {
+
+  $(".modal-completo").fadeOut(1000, function(){
+    $("body").css("overflow", "inherit");
+  });
+
+  $(".logo-menu").click(function(){
+    $(".modal-menu").fadeToggle();
+  });
+
+  // funciones del sitio anterior
   $("img").addClass("img-fluid");
   $("body.post article.post img").addClass("rounded img-thumbnail d-block mx-auto");
   $("body.home div.card p:first-of-type").addClass("m-0");
@@ -8,6 +18,6 @@ $(function() {
   /* JS puro para Twemoji */
   twemoji.parse(document.body);
 
-  
+
 
 });
